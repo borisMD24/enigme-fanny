5edbaac0913687bc871e809e283768b1f4c1e429
Boris Marion-Dorier <borismariondorier@gmail.com>
2021-02-11 11:31:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const check = (e) =>{
       <h1>  
         gg well play
       </h1>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/LM-DpnpHIak" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="40vh" height="30vw" src="https://www.youtube.com/embed/LM-DpnpHIak" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     `
   }
 }
